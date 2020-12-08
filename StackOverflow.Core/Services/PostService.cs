2367d7c857c543a154278a16c391c897d828a7d7
Ahmed Ali Fahim <fahimahmedali1@gmail.com>
2020-12-08 18:38:05 +0600

    post service cleanup

--- StackOverflow.Core/Services/PostService.cs
+++ StackOverflow.Core/Services/PostService.cs
@@ -1,25 +1,12 @@
 ﻿using StackOverflow.Core.Entities;
 using StackOverflow.Core.Repositories;
-using StackOverflow.Core.UnitOfWorks;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace StackOverflow.Core.Services
 {
     public class PostService : IPostService
     {
-        //private readonly IUnitOfWork _unitOfWork;
         private readonly IPostRepository _postRepository;
-
-        //public PostService(IUnitOfWork unitOfWork)
-        //{
-        //    _unitOfWork = unitOfWork;
-        //}
-
-        public PostService()
-        {
-            _postRepository = new PostRepository();
-        }
 
         public PostService(IPostRepository postRepository)
         {
@@ -29,9 +16,6 @@ namespace StackOverflow.Core.Services
         public void Create(Post post)
         {
             _postRepository.Create(post);
-            //_unitOfWork.BeginTransaction();
-            //_unitOfWork.PostRepository.Create(post);
-            //_unitOfWork.Commit();
         }
 
         public void Delete(int id)
@@ -42,13 +26,11 @@ namespace StackOverflow.Core.Services
         public IList<Post> GetAll()
         {
             return _postRepository.Get();
-            //return _unitOfWork.PostRepository.Get().ToList();
         }
 
         public Post GetById(int id)
         {
             return _postRepository.Get(id);
-            //return _unitOfWork.PostRepository.Get(id);
         }
 
         public void Update(Post post)
